c41b96ab553ab1ca9051907831906b09d092abdc
lparolari <luca.parolari23@gmail.com>
2023-01-03 16:42:53 +0100

    docs: add help in statement cli

--- statement/cli.py
+++ statement/cli.py
@@ -9,17 +9,33 @@ from statement.vocab import Vocab
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input")
-    parser.add_argument("output")
+    parser = argparse.ArgumentParser(
+        description="Process a CSV bank statement to a Notion Cashflow Manager importable dataset"
+    )
+    parser.add_argument("input", help="Path to statement to process")
+    parser.add_argument("output", help="Path to processed statement")
     parser.add_argument(
-        "--processor", choices=["revolut", "intesa", "vivid"], required=True
+        "--processor",
+        choices=["revolut", "intesa", "vivid"],
+        required=True,
+        help="Statement processor to use",
     )
     parser.add_argument(
-        "--category-vocab-path", default=str("assets/category_vocab.json")
+        "--category-vocab-path",
+        default=str("assets/category_vocab.json"),
+        help="Path to a JSON file containing the category vocabulary",
     )
-    parser.add_argument("--budget-vocab-path", default=str("assets/budget_vocab.json"))
-    parser.add_argument("--retrain", default=False, action="store_true")
+    parser.add_argument(
+        "--budget-vocab-path",
+        default=str("assets/budget_vocab.json"),
+        help="Path to a JSON file containing the budget vocabulary",
+    )
+    parser.add_argument(
+        "--retrain",
+        default=False,
+        action="store_true",
+        help="If true, prompt the user to insert out of vocabulary words for both categories and budgets",
+    )
 
     args = parser.parse_args()
 
